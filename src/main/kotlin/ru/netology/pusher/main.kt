f7674797db60d596f768b553cd77d7cf16134203
prince-tagaev <asaba@internet.ru>
2022-08-26 19:56:03 +0300

    deleted comments

--- src/main/kotlin/ru/netology/pusher/main.kt
+++ src/main/kotlin/ru/netology/pusher/main.kt
@@ -8,19 +8,14 @@ import com.google.firebase.messaging.Message
 import java.io.FileInputStream
 
 fun main() {
-    val topic = "/topics/FOOTBALL"
     val options = FirebaseOptions.builder()
         .setCredentials(GoogleCredentials.fromStream(FileInputStream("fcm.json")))
         .setDatabaseUrl(dbUrl)
         .build()
 
     FirebaseApp.initializeApp(options)
-
-// See documentation on defining a message payload.
-
-// See documentation on defining a message payload.
     val message = Message.builder()
-        .putData("action", "Like")
+        .putData("action", "Lie")
         .putData("content", """{
             "userId": 1,
             "userName": "admin",
@@ -30,12 +25,6 @@ fun main() {
         """.trimMargin())
         .setToken(token)
         .build()
-
-// Send a message to the devices subscribed to the provided topic.
-
-// Send a message to the devices subscribed to the provided topic.
     val response = FirebaseMessaging.getInstance().send(message)
-// Response is a message ID string.
-// Response is a message ID string.
     println("Successfully sent message: $response")
 }
