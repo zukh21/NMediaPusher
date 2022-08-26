ce1a45bba379994e6a91f516fc0f04bd12d8b215
prince-tagaev <asaba@internet.ru>
2022-08-26 22:52:38 +0300

    initial commit for Push new post pusher

--- src/main/kotlin/ru/netology/pusher/main.kt
+++ src/main/kotlin/ru/netology/pusher/main.kt
@@ -8,22 +8,24 @@ import com.google.firebase.messaging.Message
 import java.io.FileInputStream
 
 fun main() {
+    val topic = "/topics/FOOTBALL"
     val options = FirebaseOptions.builder()
         .setCredentials(GoogleCredentials.fromStream(FileInputStream("fcm.json")))
         .setDatabaseUrl(dbUrl)
         .build()
 
     FirebaseApp.initializeApp(options)
+
     val message = Message.builder()
-        .putData("action", "Lie")
-        .putData("content", """{
-            "userId": 1,
-            "userName": "admin",
-            "postId": 2,
-            "postAuthor": "Netology"
+        .putData("post", """{
+            "id": 1,
+            "author": "Netology",
+            "content": "this is content this is content this is content this is content this is content this is content this is content this is content this this is content is content",
+            "publishedDate": "now",
+            "videoLink": "https://image.winudf.com/v2/image/Y29tLmZydWl0d2FsbHBhcGVyLmhkLmZydWl0cGljdHVyZXMucGhvdG9zLmJhY2tncm91bmQuY3V0ZS5jb29sLmFydC5mcnVpdGltYWdlcy5oZC5mcmVlX3NjcmVlbl8wXzE1MzIwODEwMzdfMDY0/screen-0.jpg?fakeurl=1&type=.jpg"
             }
         """.trimMargin())
-        .setToken(token)
+        .setTopic(topic)
         .build()
     val response = FirebaseMessaging.getInstance().send(message)
     println("Successfully sent message: $response")
